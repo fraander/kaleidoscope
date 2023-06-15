adbf3f82711c2e44b4d480afc5e9cd9d8df3b0e6
fraander <57777918+fraander@users.noreply.github.com>
2023-06-15 09:50:34 -0400

    continuing to build out landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,40 @@ export default function Home() {
     <div className="bg-khoury-light h-8 w-8 rounded-full shadow-md"></div>
   );
   const circleGrid = (
-    <div className="flex flex-col gap-3 p-2">
+    <div className="flex flex-col gap-6 p-2">
       {items.map((i) => {
         return (
-          <div className="flex flex-row gap-3">{items.map((i) => circle)}</div>
+          <div className="flex flex-row gap-6">{items.map((i) => circle)}</div>
         );
       })}
     </div>
+  );
+
+  const header = (
+    <div className="flex flex-row items-center justify-center">
+      <img
+        src="kaleidoscope-640.png"
+        alt="kaleidoscope logo"
+        className="w-20 h-20 drop-shadow-md"
+      />
+      <h1 className="text-3xl font-bold text-khoury-blue drop-shadow-md cursor-default">
+        {strings.home.club_name}
+      </h1>
+    </div>
+  );
+
+  const menu = (
+    <ul>
+      {strings.home.links.map((link, index) => (
+        <li
+          className={
+            "list-[square] text-khoury-secondary drop-shadow-md hover:translate-x-3 transition-all duration-200 hover:scale-110 hover:drop-shadow-xl hover:font-bold hover:text-khoury-blue"
+          }
+        >
+          <a href={link.destination}>{link.title}</a>
+        </li>
+      ))}
+    </ul>
   );
 
   return (
@@ -21,21 +48,13 @@ export default function Home() {
       <div className="absolute left-0">{circleGrid}</div>
       <div className="absolute right-0 bottom-0">{circleGrid}</div>
       <div className="flex flex-col justify-center items-center gap-2 w-full h-full">
-        <h1 className="text-3xl font-bold text-khoury-light drop-shadow-md cursor-default">
-          {strings.home.club_name}
-        </h1>
-        <p className="italic text-khoury-light drop-shadow-md cursor-default">
-          {strings.home.tagline}
-        </p>
-        <ul>
-          {strings.home.links.map((link, index) => (
-            <li className={"list-[square] text-khoury-light drop-shadow-md hover:translate-x-3 transition-all duration-200 hover:scale-110 hover:drop-shadow-xl"}>
-              <a href={link.destination}>
-                {link.title}
-              </a>
-            </li>
-          ))}
-        </ul>
+        <div className="flex flex-col justify-center items-center gap-2 bg-white w-full py-8 shadow-2xl">
+          {header}
+          <p className="italic text-khoury-dark drop-shadow-md cursor-default">
+            {strings.home.tagline}
+          </p>
+          {menu}
+        </div>
       </div>
     </div>
   );
