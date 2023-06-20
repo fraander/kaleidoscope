05cad4b6c1d21d29d47033fa7b76e5c3b44ca934
ninest <parth.kabra@gmail.com>
2023-06-20 19:09:51 -0400

    Add club sample information and contact info

---
+++ src/app/utils/clubs.ts
@@ -0,0 +1,53 @@
+// TODO: get actual list of clubs from Notion
+// Remove this when no longer needed
+export const clubs = [
+  {
+    slug: "code4community",
+    name: "Code4Community",
+    email: "c4cneu@gmail.com",
+    website: "https://www.c4cneu.com/",
+    president: {
+      name: "Somya Prabhakar",
+      email: "prabhakar.so@northeastern.edu",
+    },
+    contact: {
+      name: "Somya Prabhakar",
+      email: "prabhakar.so@northeastern.edu",
+    },
+  },
+  {
+    slug: "colorstack",
+    name: "ColorStack",
+    president: {
+      name: "Levin Sanchez",
+      email: "sanchez.le@northeastern.edu",
+    },
+  },
+  {
+    slug: "ner",
+    name: "NER",
+    email: "northeasternelectricracing@gmail.com",
+    website: "https://electricracing.northeastern.edu",
+    president: {
+      name: "Catherine Kennedy",
+    },
+    contact: {
+      name: "Anthony Bernardi",
+      email: "bernardi.an@northeastern.edu",
+    },
+  },
+  {
+    slug: "sandbox",
+    name: "Sandbox",
+    email: "sandboxneu@gmail.com",
+    website: "https://www.sandboxnu.com/",
+    president: {
+      name: "Ally Chao",
+      email: "chao.al@northeastern.edu",
+    },
+    contact: {
+      name: "Ally Chao",
+      email: "chao.al@northeastern.edu",
+    },
+  },
+];
